100eaf733a5d4231bc1b14125afc6b5ffa462016
Gunwoo Cho <gunwoo@hancommds.com>
2019-02-22 09:19:58 +0900

    [수정] 미사용 코드 제거

--- public/app-thingspin-fms/angular-modules/tsAngular_wrapper.ts
+++
@@ -1,7 +0,0 @@
-import { tsReact2AngularDirective } from 'app-thingspin-fms/utils/tsReact2Angular';
-
-import { TsSideMenu } from 'app-thingspin-fms/react/components/TsSideMenu';
-
-export function tsRegisterAngularDirectives() {
-  tsReact2AngularDirective('tsSidemenu', TsSideMenu, []);
-}
