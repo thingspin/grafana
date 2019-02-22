2cf69040936a246d3ad1592e55f13ef05825042f
Gunwoo Cho <gunwoo@hancommds.com>
2019-02-22 09:19:22 +0900

    [추가] angular directive override 방법 추가

---
+++ public/app-thingspin-fms/angular-modules/thingspin/directives/CustomDirective/index.ts
@@ -0,0 +1,26 @@
+import angular from 'angular';
+import { SearchCtrl } from 'app/core/components/search/search';
+import { coreModule } from 'app/core/core';
+import { multipleDirectiveSelector } from '../tsReact2Angular';
+
+export class TsSearchCtrl extends SearchCtrl {
+  /** @ngInject */
+  constructor($scope, $location, $timeout, searchSrv) {
+    super($scope, $location, $timeout, searchSrv);
+  }
+}
+export function tsSearchDirective() {
+  return {
+    restrict: 'E',
+    templateUrl: 'public/app/core/components/search/search.html',
+    controller: TsSearchCtrl,
+    bindToController: true,
+    controllerAs: 'ctrl',
+    scope: {},
+  };
+}
+
+// angular.module("thingspin.directives").directive("DashboardSearch", tsSearchDirective);
+angular.module('thingspin.directives').directive('dashboardSearch', tsSearchDirective);
+// directive가 여러개가 정의 된 경우 사용자 선택 directive 사용
+coreModule.decorator('dashboardSearchDirective', multipleDirectiveSelector);
--- public/app-thingspin-fms/angular-modules/thingspin/directives/tsReact2Angular.ts
+++ public/app-thingspin-fms/angular-modules/thingspin/directives/tsReact2Angular.ts
@@ -10,3 +10,14 @@ export function tsReact2AngularDirective(name: string, component: any, options: 
     },
   ]);
 }
+
+// ref : https://gist.github.com/bennadel/647bf5b82b9d4332e8f0
+// ref : https://mobicon.tistory.com/329
+export function multipleDirectiveSelector($delegate) {
+  for (const mod of $delegate) {
+    if (mod.$$moduleName === 'thingspin.directives') {
+      return [mod];
+    }
+  }
+  return [$delegate[0]];
+}
