b564d757dbcd8f53da9835efb0ab79b7b6b00156
김상수 <sangsoo.kim@hancommds.com>
2019-02-27 23:30:48 +0900

    [추가] 로그인 라우트 오버라이드

--- public/app-thingspin-fms/routes/routes.ts
+++ public/app-thingspin-fms/routes/routes.ts
@@ -1,5 +1,6 @@
 import 'app/routes/dashboard_loaders';
 import 'app/routes/ReactContainer';
+import { applyRouteRegistrationHandlers } from 'app/routes/registry';
 
 // Types
 import { setupAngularRoutes } from 'app/routes/routes';
@@ -7,4 +8,14 @@ import { setupAngularRoutes } from 'app/routes/routes';
 /** @ngInject */
 export function fmsSetupAngularRoutes($routeProvider, $locationProvider) {
   setupAngularRoutes($routeProvider, $locationProvider);
+
+  $locationProvider.html5Mode(true);
+  $routeProvider
+    .when('/login', {
+      templateUrl: 'public/app-thingspin-fms/partials/login.html',
+      controller: 'LoginCtrl',
+      pageClass: 'login-page sidemenu-hidden',
+    });
+
+  applyRouteRegistrationHandlers($routeProvider);
 }
