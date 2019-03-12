32e4cd73e4715ebb7fd47dd4ce1fe4448f1bd625
조건우 <gunwoo@hancommds.com>
2019-03-12 17:49:27 +0900

    TS30DEV-13 [추가] 레이어 추가 및 설정에 따라 보이기/숨기기 기능 추가

--- public/app-thingspin-fms/react/tsAngular_wrapper.ts
+++ public/app-thingspin-fms/react/tsAngular_wrapper.ts
@@ -3,10 +3,12 @@ import { TsNavbarComponent } from './components/Navbar';
 import { TsLeftSidebar } from './components/LeftSidebar';
 import { TsRightSidebar } from './components/RightSidebar';
 import { TsBottombar } from './components/Bottombar';
+import { TsToolbarComponent } from './components/MainView/Toobar/index';
 
 export function tsRegisterAngularDirectives() {
   tsReact2AngularDirective('tsNavbar', TsNavbarComponent, []);
   tsReact2AngularDirective('tsLeftSidebar', TsLeftSidebar, []);
   tsReact2AngularDirective('tsRightSidebar', TsRightSidebar, []);
   tsReact2AngularDirective('tsBottombar', TsBottombar, []);
+  tsReact2AngularDirective('tsToolbar', TsToolbarComponent, []);
 }
