e27d7e75de5bfecc6568f2e155333beee32893c0
조건우 <gunwoo@hancommds.com>
2019-04-04 16:37:47 +0900

    TS30DEV-14 [수정] 외부 링크 테이블 명 수저

--- pkg/services/sqlstore/migrations/thingspin/fms_ext_link.go
+++ pkg/services/sqlstore/migrations/thingspin/fms_ext_link.go
@@ -1,12 +1,15 @@
 package thingspin
 
 import (
+	"fmt"
+
+	tsm "github.com/grafana/grafana/pkg/models-thingspin"
 	. "github.com/grafana/grafana/pkg/services/sqlstore/migrator"
 )
 
 func addFmsExtLinkMigrations(mg *Migrator) {
-	query := `
-		CREATE TABLE IF NOT EXISTS 'TS_FMS_EXT_LINK' (
+	query := fmt.Sprintf(`
+		CREATE TABLE IF NOT EXISTS '%s' (
 			"id" integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 			"uid" integer references user(id),
 			"url" text,
@@ -14,7 +17,7 @@ func addFmsExtLinkMigrations(mg *Migrator) {
 			"CREATED_DATE" datetime default (datetime('now', 'localtime')),
 			"UPDATED_DATE" datetime default (datetime('now', 'localtime'))
 		)
-	`
+	`, tsm.TsFmsExtLinkTbl)
 
 	// create table
 	mg.AddMigration("[thingspin] FMS 외부 링크 테이블 생성", NewRawSqlMigration(query))
