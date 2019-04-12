a77cf49adc77c29afd9a571fd671c353f3504977
조건우 <gunwoo@hancommds.com>
2019-04-12 15:53:28 +0900

    TS30DEV-16 [추가] defaults_thingspin.ini 읽어 setting에 사용하는 기능 추가

--- pkg/setting/setting_thingspin.go
+++ pkg/setting/setting_thingspin.go
@@ -1,16 +1,62 @@
 package setting
 
+import (
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/grafana/grafana/pkg/util"
+	ini "gopkg.in/ini.v1"
+)
+
 var (
-	Thingspin ThingspinSettings
+	Thingspin  ThingspinSettings
+	TsRaw      *ini.File
+	TsInitPath = "conf/defaults_thingspin.ini"
 )
 
 type ThingspinSettings struct {
-	Enabled bool
+	Enabled           bool
+	NodeRedHost       string
+	NodeRedModuleList []string
+}
+
+func (cfg *Cfg) loadTsIniFile() error {
+	// load config defaults
+	defaultTsConfigFile := path.Join(HomePath, TsInitPath)
+	configFiles = append(configFiles, defaultTsConfigFile)
+
+	// check if config file exists
+	if _, err := os.Stat(defaultTsConfigFile); os.IsNotExist(err) {
+		fmt.Println("Grafana-server Init Failed: Could not find config defaults, make sure homepath command line parameter is set or working directory is homepath")
+		os.Exit(1)
+	}
+
+	// load defaults
+	parsedFile, err := ini.Load(defaultTsConfigFile)
+	if err != nil {
+		fmt.Println(fmt.Sprintf("Failed to parse defaults_thingspin.ini, %v", err))
+		os.Exit(1)
+		return err
+	}
+
+	TsRaw = parsedFile
+
+	return nil
 }
 
 func (cfg *Cfg) readThingspinSettings() {
-	sec := cfg.Raw.Section("thingspin")
+	cfg.loadTsIniFile()
+
+	sec := TsRaw.Section("thingspin")
 	Thingspin.Enabled = sec.Key("enabled").MustBool(true)
+
+	nrSec := TsRaw.Section("node-red")
+	Thingspin.NodeRedHost = nrSec.Key("host").String()
+
+	for _, module := range util.SplitString(nrSec.Key("modules").String()) {
+		Thingspin.NodeRedModuleList = append(Thingspin.NodeRedModuleList, module)
+	}
 
 	cfg.Thingspin = Thingspin
 }
