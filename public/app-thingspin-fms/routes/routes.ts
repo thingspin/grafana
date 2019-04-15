4cf6e1928568db1a629d6aceb5ff53f8339f6a4a
조건우 <gunwoo@hancommds.com>
2019-04-15 10:43:59 +0900

    TS30DEV-16 [추가] front-end routing 초안 추가

--- public/app-thingspin-fms/routes/routes.ts
+++ public/app-thingspin-fms/routes/routes.ts
@@ -14,7 +14,28 @@ export function fmsSetupAngularRoutes($routeProvider, $locationProvider) {
     templateUrl: 'public/app-thingspin-fms/partials/login.html',
     controller: 'LoginCtrl',
     pageClass: 'login-page sidemenu-hidden',
-  }).when('/thingspin-page/system-management/menu-management', {
-    template: '<ts-menu-management />',
-  });
+  })
+    .when('/thingspin-page/alarm-management', { template: '<ts-alarm-management />', })
+
+    .when('/thingspin-page/analysis-management', { template: '<ts-analysis-management />', })
+    .when('/thingspin-page/analysis-management/correlation-analysis', { template: '<ts-corr-analysis />', })
+    .when('/thingspin-page/analysis-management/pattern-analysis', { template: '<ts-pattern-analysis />', })
+    .when('/thingspin-page/analysis-management/predition-analysis', { template: '<ts-prediction-analysis />', })
+    .when('/thingspin-page/analysis-management/scrip-analysis', { template: '<ts-script-analysis />', })
+
+    .when('/thingspin-page/data-management', { template: '<ts-data-management />', })
+    .when('/thingspin-page/data-management/connect-management', { template: '<ts-connect-management />', })
+    .when('/thingspin-page/data-management/connect-management/opcua', { template: '<ts-opcua-connect />', })
+    .when('/thingspin-page/data-management/connect-management/modbus', { template: '<ts-modbus-connect />', })
+    .when('/thingspin-page/data-management/connect-management/mqtt', { template: '<ts-mqtt-connect />', })
+
+    .when('/thingspin-page/facility-management', { template: '<ts-facility-management />', })
+
+    .when('/thingspin-page/facility-monitoring', { template: '<ts-facility-monitoring />', })
+
+    .when('/thingspin-page/system-management', { template: '<ts-system-management />', })
+    .when('/thingspin-page/system-management/user-management', { template: '<ts-user-management />', })
+    .when('/thingspin-page/system-management/menu-management', { template: '<ts-menu-management />', })
+
+    .when('/thingspin-page/view-edit-management', { template: '<ts-view-edit-management />', });
 }
