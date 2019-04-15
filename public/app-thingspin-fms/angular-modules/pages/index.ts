f07be18d82d5b21e7d6591522a80574d30b00dad
조건우 <gunwoo@hancommds.com>
2019-04-15 10:26:32 +0900

    TS30DEV-16 [추가] 메뉴별 directive를 thingspin에서 사용할 수 있도록 추가

--- public/app-thingspin-fms/angular-modules/pages/index.ts
+++ public/app-thingspin-fms/angular-modules/pages/index.ts
@@ -1,1 +1,23 @@
+import './alarmManagement';
+
+import './analysisManagement';
+import './analysisManagement/correlationAnalysis';
+import './analysisManagement/patternAnalysis';
+import './analysisManagement/predictionAnalysis';
+import './analysisManagement/scriptAnalysis';
+
+import './dataManagement';
+import './dataManagement/connectManagement';
+import './dataManagement/connectManagement/modbus';
+import './dataManagement/connectManagement/mqtt';
+import './dataManagement/connectManagement/mqtt';
+
+import './facilityManagement';
+
+import './facilityMonitoring';
+
+import './systemManagement';
 import './systemManagement/menuManagement';
+import './systemManagement/userManagement';
+
+import './viewEditManagement';
