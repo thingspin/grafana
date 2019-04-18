a24e3d11a120df3ddbfb8867897a695b34de1ead
조건우 <gunwoo@hancommds.com>
2019-04-18 13:51:37 +0900

    TS30DEV-16 [수정] 연결 이름 빠지고 가져오는 오류 수정

--- pkg/models-thingspin/fmsConnect.go
+++ pkg/models-thingspin/fmsConnect.go
@@ -12,6 +12,7 @@ type TsConnectReq struct {
 
 type TsConnectField struct {
 	Id      int                    `json:"id"`
+	Name    string                 `json:"name"`
 	FlowId  string                 `json:"flow_id"`
 	Type    string                 `json:"type"`
 	Params  map[string]interface{} `json:"params"`
