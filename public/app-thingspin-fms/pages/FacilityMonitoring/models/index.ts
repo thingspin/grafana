b92b2ea8adcd4caad8f49504a09cb6ea1fe6dd8f
조건우 <gunwoo@hancommds.com>
2019-06-19 17:32:04 +0900

    TS30DEV-28 [추가] 대시보드가 가진 설비 정보 전달 기능 추가

---
+++ public/app-thingspin-fms/pages/FacilityMonitoring/models/index.ts
@@ -0,0 +1,12 @@
+import { DashboardModel } from 'app/features/dashboard/state';
+import { DashboardMeta } from 'app/types';
+
+
+export class FMDashboardModel extends DashboardModel {
+    facilityTags: any;
+    site: any;
+
+    constructor(data: any, meta?: DashboardMeta) {
+        super(data, meta);
+    }
+}
