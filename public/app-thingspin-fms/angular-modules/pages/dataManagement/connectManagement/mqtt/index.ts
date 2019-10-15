a44c0b4d54982cd3b6d5bce79b8c958bbc56a727
조건우 <gunwoo@hancommds.com>
2019-10-15 10:10:49 +0900

    TS30DEV-35 [수정] MQTT 코드 정리

--- public/app-thingspin-fms/angular-modules/pages/dataManagement/connectManagement/mqtt/index.ts
+++ public/app-thingspin-fms/angular-modules/pages/dataManagement/connectManagement/mqtt/index.ts
@@ -1,10 +1,15 @@
+// JS 3rd party libs
 import _ from "lodash";
-import "./index.scss";
-import { BackendSrv } from 'app/core/services/backend_srv';
 import angular, { ITimeoutService } from 'angular';
-import { appEvents } from 'app/core/core';
 const uid = require("shortid");
+import "./index.scss";
 
+// Grafana libs
+import { dateTime } from '@grafana/data';
+import { appEvents } from 'app/core/core';
+import { BackendSrv } from 'app/core/services/backend_srv';
+
+// Thingspin libs
 import TsMqttController from 'app-thingspin-fms/utils/mqttController';
 
 const DEF_URL = "localhost";
@@ -50,24 +55,8 @@ export interface TableModel {
   maxPageLen: number; // paging 최대 표시 개수
 }
 
-
-function _formatDatetime(date: Date, format: string) {
-  const _padStart = (value: number): string => value.toString().padStart(2, '0');
-return format
-   .replace(/yyyy/g, _padStart(date.getFullYear()))
-   .replace(/dd/g, _padStart(date.getDate()))
-   .replace(/mm/g, _padStart(date.getMonth() + 1))
-   .replace(/hh/g, _padStart(date.getHours()))
-   .replace(/ii/g, _padStart(date.getMinutes()))
-   .replace(/ss/g, _padStart(date.getSeconds()));
-}
-function isValidDate(d: Date): boolean {
-   return !isNaN(d.getTime());
-}
-
-export function formatDate(date: any): string {
-  const datetime = new Date(date);
-  return isValidDate(datetime) ? _formatDatetime(datetime, 'yyyy-mm-dd_hh:ii:ss') : '';
+export function formatDate(date: Date): string {
+  return dateTime(date).format('YYYY-MM-DD_HH:mm:ss');
 }
 
 export class TsMqttConnectCtrl {
@@ -247,7 +236,7 @@ export class TsMqttConnectCtrl {
     return true;
   }
 
-  connectTest () {
+  connectTest() {
     if (this.inputChecker()) {
       if (this.indexID === -1 || this.tableList.size === 0) {
         if (this.topicDisListArrayString.length === 0) {
@@ -262,40 +251,6 @@ export class TsMqttConnectCtrl {
     if (this.inputChecker()) {
       this.onJsonCreatSender(true);
     }
-    // if (value) {
-    //   if (this.collector && this.connection.url && this.connection.port && this.connection.keep_alive) {
-    //     this.onJsonCreatSender(true);
-    //   } else {
-    //     if (this.inputChecker()) {
-
-    //     }
-    //   }
-    // } else {
-    //   if (!this.collector) {
-    //     this.openAlartNotification("수집기 이름을 입력해주세요.");
-    //     return;
-    //   } else if (!this.connection.url) {
-    //     this.openAlartNotification("HOST를 입력해주세요.");
-    //     return;
-    //   } else if (!this.connection.port) {
-    //     this.openAlartNotification("PORT를 입력해주세요.");
-    //     return;
-    //   } else if (!this.connection.keep_alive) {
-    //     this.openAlartNotification("Keep Alive 초를 입력해주세요.");
-    //     return;
-    //   }
-
-    //   if (this.indexID === -1 || this.tableList.size === 0) {
-    //     if (this.topicDisListArrayString.length === 0) {
-    //       this.createConnectNode();
-    //     }
-    //     this.methodProcess(this.createHttpObject(), false);
-    //   } else {
-    //     if (value) {
-    //       this.onJsonCreatSender(value);
-    //     }
-    //   }
-    // }
   }
 
   onShowTopicEditView(value: any) {
@@ -826,6 +781,7 @@ export class TsMqttConnectCtrl {
         keep_alive: dash.connection.keep_alive,
         session: dash.connection.session,
       };
+
       dash.topicList.forEach(({ name: n, type, viewStr, topic, value }: any, id: number) => {
         const tableData: Topic = {
           id,
@@ -840,30 +796,22 @@ export class TsMqttConnectCtrl {
     }
   }
 
-  jsonDataParsingChecker(data: any) {
-    if (data.collector === undefined) {
-      return false;
-    } else if (data.connection === undefined) {
-      return false;
-    } else if (data.topicList === undefined) {
-      return false;
-    }
-    return true;
+  jsonDataParsingChecker({collector, connection, topicList}: any) {
+      return !(!collector || !connection || !topicList);
   }
 
   async exportData() {
     const outputData = {
-      "collector": this.collector,
-      "connection": this.connection,
-      "topicList": Array.from(this.tableList.values())
+      collector: this.collector,
+      connection: this.connection,
+      topicList: Array.from(this.tableList.values())
     };
 
     const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(outputData));
-    $("#downloadAnchorElem").attr("href", dataStr);
-    $("#downloadAnchorElem").attr("download", this.collector + "_" + formatDate(new Date()) + ".json");
-    $("#downloadAnchorElem").get(0).click();
-
-    // console.log(outputData);
+    const $elem = $("#downloadAnchorElem");
+    $elem.attr("href", dataStr);
+    $elem.attr("download", this.collector + "_" + formatDate(new Date()) + ".json");
+    $elem.get(0).click();
   }
 }
 export function tsMqttConnectDirective() {
--- public/app-thingspin-fms/angular-modules/pages/dataManagement/connectManagement/uploadJson/uploadJsonDirective.ts
+++ public/app-thingspin-fms/angular-modules/pages/dataManagement/connectManagement/uploadJson/uploadJsonDirective.ts
@@ -3,17 +3,22 @@ import appEvents from 'app/core/app_events';
 import angular, { ILocationService } from 'angular';
 
 const template = `
-<input type="file" id="jsonupload" name="dashupload" class="hide" onchange="angular.element(this).scope().fileSelected()"/>
+<input type="file" id="jsonupload" name="dashupload" class="hide" onchange="angular.element(this).scope().fileSelected"/>
 <label id="mqtt-cancel-btn" class="btn" for="jsonupload">
   {{btnText}}
 </label>
 `;
+
+interface HTMLInputEvent extends Event {
+  target: HTMLInputElement & EventTarget;
+}
 
 /** @ngInject */
 export function uploadJsonDirective(timer: any, $location: ILocationService) {
   return {
     restrict: 'E',
     template: template,
+    controllerAs: 'ctrl',
     scope: {
       onUpload: '&',
       btnText: '@?',
@@ -21,36 +26,25 @@ export function uploadJsonDirective(timer: any, $location: ILocationService) {
     link: (scope: any, elem: any) => {
       scope.btnText = angular.isDefined(scope.btnText) ? scope.btnText : 'Upload .json file';
 
-      scope.fileSelected = function (evt: any) {
-        const files = evt.target.files; // FileList object
-        const readerOnload = () => {
-          return (e: any) => {
-            let dash: any;
-            try {
-              dash = JSON.parse(e.target.result);
-            } catch (err) {
-              console.log(err);
-              appEvents.emit('alert-error', ['Import failed', 'JSON -> JS Serialization failed: ' + err.message]);
-              return;
-            }
-
-            scope.$apply(() => {
-              scope.onUpload({ dash });
-            });
-          };
-        };
-
-        let i = 0;
-        let file = files[i];
-
-        while (file) {
-          const reader = new FileReader();
-          reader.onload = readerOnload();
-          reader.readAsText(file);
-          i += 1;
-          file = files[i];
+      function readerOnload({ target }: ProgressEvent<FileReader>) {
+        try {
+          const dash = JSON.parse(target.result as string);
+          scope.$apply(() => {
+            scope.onUpload({ dash });
+          });
+        } catch (err) {
+          console.log(err);
+          appEvents.emit('alert-error', ['Import failed', 'JSON -> JS Serialization failed: ' + err.message]);
         }
       }
+
+      scope.fileSelected = ({ target: { files } }: HTMLInputEvent) => {
+        for (let i = 0; i < files.length; i += 1) {
+          const reader = new FileReader();
+          reader.onload = readerOnload;
+          reader.readAsText(files[i]);
+        }
+      };
 
       const wnd: any = window;
       // Check for the various File API support.
