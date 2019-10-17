dd14346cdef9d41bdb5bee743b40116aa6286c4e
조건우 <gunwoo@hancommds.com>
2019-10-17 13:39:06 +0900

    TS30DEV-2 [수정] 시스템 로그 미개발에 따른 화면 제거

--- public/app-thingspin-fms/react/components/RightSidebar/index.tsx
+++ public/app-thingspin-fms/react/components/RightSidebar/index.tsx
@@ -4,42 +4,29 @@ import React from 'react';
 // thingspin react components
 import Tabs from './Tabs';
 import Tab from './Tab';
-
-// thingspin react components
-import { TsBaseProps } from 'app-thingspin-fms/models/common';
 import { FmsAlarmBandComp } from './FmsAlarmBand';
 
-export interface Props extends TsBaseProps {
-}
+const getTabNode = (title: string, icon: string) => () => (<>
+  <i className={`fa ${icon} fa-2`} />
+  <span className="fms-right-tap-alarm-title">
+    {title}
+  </span>
+</>);
 
-function getTabNode(title: string, icon: string): () => React.ReactNode {
-  return () => {
-    return <>
-      <i className={`fa ${icon} fa-2`} />
-      <span className="fms-right-tap-alarm-title">
-        {title}
-      </span>
-    </>;
-  };
-}
+const tabs = [
+  <Tab
+    name="alarm" initActive={true}
+    heading={getTabNode('알람', "fms-right-tap-alarm-icon fa-bell")}
+  >
+    <FmsAlarmBandComp />
+  </Tab>,
+  /*
+  <Tab
+    name="log"
+    heading={getTabNode('시스템 로그', "fms-right-tap-alarm-icon fa-window-maximize")}
+  >
+  </Tab>
+  */
+];
 
-export const TsRightSidebar: React.FC<Props> = (_) => {
-  return (<Tabs>
-    {/* Alarm Log Tab */}
-    <Tab
-      name="alarm" initActive={true}
-      heading={getTabNode('알람', "fms-right-tap-alarm-icon fa-bell")}
-    >
-      <FmsAlarmBandComp />
-    </Tab>
-
-    {/* System Log Tab */}
-    <Tab
-      name="log"
-      heading={getTabNode('시스템 로그', "fms-right-tap-alarm-icon fa-window-maximize")}
-    >
-    </Tab>
-
-  </Tabs>
-  );
-};
+export const TsRightSidebar: React.FC = () => <Tabs>{tabs}</Tabs>;
