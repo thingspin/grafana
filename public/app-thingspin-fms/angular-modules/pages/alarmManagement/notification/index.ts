17d12f09c4145f93b28e5e44440b2feceb9e7e2c
조건우 <gunwoo@hancommds.com>
2019-10-18 11:04:36 +0900

    TS30DEV-35 [수정] 알람 발송 관리 중복 코드 및 불필요 주석 제거

--- public/app-thingspin-fms/angular-modules/pages/alarmManagement/notification/index.ts
+++ public/app-thingspin-fms/angular-modules/pages/alarmManagement/notification/index.ts
@@ -4,7 +4,7 @@ import _ from "lodash";
 // Grafana libs
 import { appEvents } from 'app/core/core';
 import { AppEvents } from '@grafana/data';
-import { coreModule, NavModelSrv } from 'app/core/core';
+import { coreModule } from 'app/core/core';
 import { BackendSrv } from 'app/core/services/backend_srv';
 
 // Thingspin libs
@@ -24,104 +24,100 @@ export interface TableModel {
 }
 
 export class TsAlarmNotiManagementCtrl {
-  //notifications: any;
-  navModel: any;
-  list: any;
+  private _list: any[];
+  set list(list: any[]) {
+    this._list = list;
+    this.tCalcPaging();
+    this.setPageNodes();
+    this.$scope.$applyAsync();
+  }
+  get list() {
+    return this._list;
+  }
+
+  // Table data model
   tData: TableModel = {
-      rowCount: 10,
-      selectOpts: [10, 20, 30],
-      currPage: 0,
-      maxPage: 0,
-      maxPageLen: 10,
-      pageNode: [],
+    rowCount: 10,
+    selectOpts: [10, 20, 30],
+    currPage: 0,
+    maxPage: 0,
+    maxPageLen: 10,
+    pageNode: [],
   };
-  name: string;
-  type: string;
 
   /** @ngInject */
-  constructor(private backendSrv: BackendSrv, navModelSrv: NavModelSrv) {
+  constructor(private $scope: angular.IScope,
+    private backendSrv: BackendSrv) {
     this.loadNotifications();
-    this.navModel = navModelSrv.getNav('alerting', 'channels', 0);
-    this.list = [];
   }
 
-  loadNotifications() {
-    this.backendSrv.get(`/api/alert-notifications`).then((result: any) => {
-      this.list = result.sort((a: any, b: any) => (a.name > b.name) ? 1 : -1);
-      this.tCalcPaging();
-      this.setPageNodes();
-    });
+  async loadNotifications() {
+    this.list = (await this.backendSrv.get(`/api/alert-notifications`))
+      .sort( ({ name }: any, b: any) => name > b.name ? 1 : -1);
   }
 
-  deleteNotification(id: number) {
-    console.log("delete = " + id);
-    this.backendSrv.delete(`/api/alert-notifications/${id}`).then(() => {
-      this.list = this.list.filter((notification: any) => {
-        return notification.id !== id;
-      });
-      this.list = this.list.sort((a: any, b: any) => (a.name > b.name) ? 1 : -1);
-      this.tCalcPaging();
-      this.setPageNodes();
-    });
+  async deleteNotification(id: number) {
+    await this.backendSrv.delete(`/api/alert-notifications/${id}`);
+    this.list = this.list.filter((noti: any) => noti.id !== id);
   }
 
   setPageNodes() {
     const { currPage, rowCount, } = this.tData;
     if (this.list) {
-        this.tData.pageNode = this.list.slice(
-            currPage * rowCount,
-            (currPage * rowCount) + rowCount
-        );
+      this.tData.pageNode = this.list.slice(
+        currPage * rowCount,
+        (currPage * rowCount) + rowCount
+      );
     }
   }
 
   tNextPaging(): void {
     if (this.tData.currPage < this.tData.maxPage) {
-        this.tData.currPage += 1;
-        this.setPageNodes();
+      this.tData.currPage += 1;
+      this.setPageNodes();
     }
   }
 
   tPrevPaging(): void {
-      if (this.tData.currPage) {
-          this.tData.currPage -= 1;
-          this.setPageNodes();
-      }
+    if (this.tData.currPage) {
+      this.tData.currPage -= 1;
+      this.setPageNodes();
+    }
   }
 
   tSetPaging(index: number) {
-      this.tData.currPage = index;
-      this.tCalcPaging();
-      this.setPageNodes();
+    this.tData.currPage = index;
+    this.tCalcPaging();
+    this.setPageNodes();
   }
 
   tCalcPaging() {
-      const { rowCount } = this.tData;
-      const temp: number = (this.list.length && (this.list.length % rowCount) === 0) ? 1 : 0;
-      this.tData.maxPage = Math.floor(this.list.length / (rowCount)) - temp;
+    const { rowCount } = this.tData;
+    const temp: number = (this.list.length && (this.list.length % rowCount) === 0) ? 1 : 0;
+    this.tData.maxPage = Math.floor(this.list.length / (rowCount)) - temp;
   }
 
   tGetPagingNumberArray() {
-      const { currPage, maxPageLen, maxPage } = this.tData;
-      const index = Math.floor(currPage / maxPageLen);
+    const { currPage, maxPageLen, maxPage } = this.tData;
+    const index = Math.floor(currPage / maxPageLen);
 
-      const from = index * maxPageLen;
-      let to = index * maxPageLen + maxPageLen;
-      if (to > maxPage) {
-          to = maxPage + 1;
-      }
+    const from = index * maxPageLen;
+    let to = index * maxPageLen + maxPageLen;
+    if (to > maxPage) {
+      to = maxPage + 1;
+    }
 
-      return _.range(from, to);
+    return _.range(from, to);
   }
+
   // table event methods
   tOnSelectChange() {
-      console.log("click");
-      this.tCalcPaging();
-      this.setPageNodes();
+    this.tCalcPaging();
+    this.setPageNodes();
   }
 
   openAlartNotification(value: any) {
-      appEvents.emit(AppEvents.alertError, [value]);
+    appEvents.emit(AppEvents.alertError, [value]);
   }
 }
 
